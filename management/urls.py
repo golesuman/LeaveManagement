afd8dde52f64e4161111dbd55b36e5de0bb48fb7
Suman Gole <gole35830@gmail.com>
2023-08-29 20:14:42 +0545

    chore: remove unnecessary comments

--- management/urls.py
+++ management/urls.py
@@ -3,11 +3,7 @@ from django.urls import path
 from .views import LeaveRequestCreateView, LoginView, HomeView
 
 urlpatterns = [
-    #     path("/login, "),
-    #     path("/logout, "),
-    #     path("/register"),
     path("", HomeView.as_view(), name="home"),
-    #     path("/request")
     path("create/", LeaveRequestCreateView.as_view(), name="create_leave"),
     path("login/", LoginView.as_view(), name="login"),
 ]
